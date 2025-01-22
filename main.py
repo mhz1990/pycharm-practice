5adff7e7556a9fc4bc254c3813408192473acb51
Matt Hoadley <matt.hoadley1990@gmail.com>
2025-01-22 21:24:02 +0800

    first pycharm commit

---
+++ main.py
@@ -0,0 +1,9 @@
+# print("Hello World")
+MyThought = "Pizza"
+TestVar = input("What am I thinking? ")
+
+if TestVar in MyThought:
+    print("Yep, I was thinking about " + MyThought)
+else:
+    print("Nope, I was thinking about " + MyThought)d
+# print("Your number is " + (TestVar + MyNum))
